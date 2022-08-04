0ddf4bdbbf33430ebd7ed2e11afec814d2cdd018
Othmar Wigger <othmar.wigger@terreactive.ch>
2022-08-04 16:20:26 +0200

    13172: package Service and ServiceCustomerUser import

--- bin/import_all_csv.sh
+++ bin/import_all_csv.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 #
-# Example script using the OTRS console ImportCVS modules e.g. for a cron job.
-# Note that order is important: import roles and users before role_user, and do on.
+# (c) terreActive AG
 #
+# Purpose: import CSV files from inventory to OTRS in the correc order
+#
+# 3456789A123456789B123456789C123456789D123456789E123456789F123456789G12345678
+#
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo usage:
+    echo "$0" --help
+    echo "$0"
+    echo "$0" --verbose
+    echo "$0" --dry-run
+    echo "$0" --dry-run --verbose
+    echo CSVDIR=/opt/Uploads "$0"
+    echo CSVDIR=/opt/Uploads "$0" --verbose
+    echo CSVDIR=/opt/Uploads "$0" --dry-run
+    echo CSVDIR=/opt/Uploads "$0" --dry-run --verbose
+    exit
+fi
+
+OPTIONS=("$@")
 CMD=/opt/otrs/bin/otrs.Console.pl
-OPTIONS=""  # "--verbose --dry-run"
-SOURCE="--source-path /opt/Uploads"
+CSVDIR=${CSVDIR-"/opt/Uploads"}
 
 do_import() {
-    echo $CMD $1 $OPTIONS $SOURCE/$2
-    $CMD $1 $OPTIONS $SOURCE/$2
+    echo $CMD "$1" "${OPTIONS[@]}" --source-path "$CSVDIR/$2"
+    $CMD "$1" "${OPTIONS[@]}" --source-path "$CSVDIR/$2"
 }
 
 do_import Admin::Group::ImportCSV groups.csv
@@ -22,5 +39,8 @@ do_import Admin::CustomerCompany::ImportCSV customer_company.csv
 do_import Admin::CustomerGroup::ImportCSV customer_group.csv
 do_import Admin::CustomerUser::ImportCSV customer_user.csv
 do_import Admin::CustomerUserCustomer::ImportCSV customer_user_customer.csv
-do_import Admin::Service::ImportCSV customer_user_customer.csv
-do_import Admin::ServiceCustomerUser::ImportCSV customer_user_customer.csv
+do_import Admin::Service::ImportCSV service.csv
+do_import Admin::ServiceCustomerUser::ImportCSV service_customer_user.csv
+#
+# EOF
+#
